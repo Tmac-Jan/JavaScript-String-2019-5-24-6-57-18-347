01cc4a5fe6df1938f0795046c844a8c85d79822f
macmanboy <macmanboy@foxmail.com>
2019-06-25 14:43:20 +0800

    update the code

--- index.js
+++ index.js
@@ -4,13 +4,24 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
+console.log(name.toLocaleUpperCase());
 
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+var arrIn = sentence.split(' ');
+var arrOut = [];
+for (var i =0;i<arrIn.length;i++){
+    arrOut.push(arrIn[i].substring(0,1).toLocaleUpperCase()+arrIn[i].substring(1));
+}
+console.log(arrOut.join(' '));
 
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
+var search='￥';
+var start = money.indexOf(search);
+var result = money.substring(start+1);
+console.log(result);
